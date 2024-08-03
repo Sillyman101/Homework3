d9104dd751ccc68ca77b08d9a773f58fb5706a9c
Sillyman101 <danilsviridov3344@gmail.com>
2024-08-03 15:59:06 -0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,10 @@
+name = 'Danil'
+print ('Name: ' + name )
+age = 21
+print ('Age: '+ str(age))
+age = age + 1
+print ('New age: ' + str(age))
+is_student = True
+print('Is Student: ' + str(is_student))
+pancakes = 'Definitely'
+print('Loves Pancakes: ' + pancakes)
